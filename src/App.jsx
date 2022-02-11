ffdf15affea214d72b1558e5bf70ca3f81500882
EdwardR0507 <ramosedward21@gmail.com>
2022-02-10 19:49:23 -0500

    refator: names and icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import ListOfGifs from "./components/ListOfGifs/ListOfGifs";
 import { usePagination } from "./hooks/usePagination";
 import Pagination from "./components/Pagination/Pagination";
-function GifExpertApp() {
-  const [category, setCategory] = useState("Aespa");
+function App() {
+  const [category, setCategory] = useState("Dreamcatcher");
   const [gifs, pageCount, changePage, loading] = usePagination({
     category,
     gifsPerPage: 9,
@@ -12,7 +12,7 @@ function GifExpertApp() {
 
   return (
     <>
-      <h1>GifExpertApp</h1>
+      <h1 className="title">Gif App</h1>
       <SearchBar setCategory={setCategory} />
       <ListOfGifs gifs={gifs} loading={loading} />
       <Pagination pageCount={pageCount} changePage={changePage} gifs={gifs} />
@@ -20,4 +20,4 @@ function GifExpertApp() {
   );
 }
 
-export default GifExpertApp;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import GifExpertApp from "./GifExpertApp";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GifExpertApp />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
