03fb2c6193005fdabac6d38b416ed6380401f9dd
EdwardR0507 <ramosedward21@gmail.com>
2022-02-10 22:45:08 -0500

    feat: add some tests

--- setupTest.js
+++
@@ -1,6 +0,0 @@
-import Enzyme from "enzyme";
-import { createSerializer } from "enzyme-to-json";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-
-expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
-Enzyme.configure({ adapter: new Adapter() });
